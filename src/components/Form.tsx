14ce62080f62ebd53dac34e464c26f33080b9b89
Fernando Chicaiza <washopilot@yahoo.com>
2022-09-20 20:17:25 -0500

    creating custom hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,49 +1,13 @@
-import { ChangeEvent, FormEvent, useReducer } from 'react';
+import { ChangeEvent, FormEvent } from 'react';
 import { Sub } from '../models';
-
-interface FormState {
-    inputValues: Sub;
-}
+import useNewSubForm from './hooks/useNewSubForm';
 
 interface IFormProps {
     onNewSub: (newSubForm: Sub) => void;
 }
 
-const INITIAL_STATE = {
-    nick: '',
-    subMonths: 0,
-    avatar: '',
-    description: ''
-};
-
-type FormReducerAction =
-    | {
-          type: 'change_value';
-          payload: {
-              inputName: string;
-              inputValue: string;
-          };
-      }
-    | {
-          type: 'clear';
-      };
-
-const formReducer = (state: FormState['inputValues'], action: FormReducerAction) => {
-    switch (action.type) {
-        case 'change_value': {
-            const { inputName, inputValue } = action.payload;
-            return {
-                ...state,
-                [inputName]: inputValue
-            };
-        }
-        case 'clear':
-            return INITIAL_STATE;
-    }
-};
-
 const Form = ({ onNewSub }: IFormProps) => {
-    const [inputValues, dispatch] = useReducer(formReducer, INITIAL_STATE);
+    const [inputValues, dispatch] = useNewSubForm();
 
     const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
---
+++ src/components/hooks/useNewSubForm.tsx
@@ -0,0 +1,45 @@
+import { useReducer } from 'react';
+import { Sub } from '../../models';
+
+interface FormState {
+    inputValues: Sub;
+}
+
+type FormReducerAction =
+    | {
+          type: 'change_value';
+          payload: {
+              inputName: string;
+              inputValue: string;
+          };
+      }
+    | {
+          type: 'clear';
+      };
+
+const INITIAL_STATE = {
+    nick: '',
+    subMonths: 0,
+    avatar: '',
+    description: ''
+};
+
+const formReducer = (state: FormState['inputValues'], action: FormReducerAction) => {
+    switch (action.type) {
+        case 'change_value': {
+            const { inputName, inputValue } = action.payload;
+            return {
+                ...state,
+                [inputName]: inputValue
+            };
+        }
+        case 'clear':
+            return INITIAL_STATE;
+    }
+};
+
+const useNewSubForm = () => {
+    return useReducer(formReducer, INITIAL_STATE);
+};
+
+export default useNewSubForm;
